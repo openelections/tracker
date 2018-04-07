57b9f93973435daec873ed62fed72b08c5c60b2c
Serdar Tumgoren <zstumgoren@gmail.com>
2018-04-07 00:31:13 -0700

    Add first vcrpy-driven tests

--- tests/integration/test_cli_repos_report.py
+++ tests/integration/test_cli_repos_report.py
@@ -1,9 +1,27 @@
+import os
+
 import pytest
 
 from tracker import cli
+import logging
 
-def test_repos_data(capsys, monkeypatch):
-    "Ensures Github access env var is set"
-    #cli.main()
-    #assert 'foo' == 'bar'
-    pass
+
+@pytest.mark.vcr(filter_headers=['authorization'])
+def test_repos_data(caplog, mocker, tmpdir):
+    # Enable below to view vcrpy debug logging
+    caplog.set_level(logging.INFO)
+    outfile = os.path.join(tmpdir.strpath, 'repos.csv')
+    parsed_args = {
+        'repos_report': True,
+        'outdir': tmpdir.strpath,
+        'publish': False
+    }
+    mock = mocker.patch(
+        'tracker.cli.get_parsed_args',
+        return_value=parsed_args,
+        autospec=True,
+    )
+    cli.main()
+    with open(outfile) as f:
+        # line count includes header
+        assert len(f.readlines()) == 120
--- tests/integration/test_graphql_repos_query.py
+++ tests/integration/test_graphql_repos_query.py
@@ -1,11 +1,10 @@
 import pytest
-from betamax import Betamax
 
 from tracker.github.graphql import ReposQuery
 
 
+@pytest.mark.vcr(filter_headers=['authorization'])
 def test_repos_query_basic():
     query = ReposQuery()
-    with Betamax(query.session).use_cassette('repos-query'):
-        repos = query.run()
-        assert len(repos) == 119
+    repos = query.run()
+    assert len(repos) == 119
